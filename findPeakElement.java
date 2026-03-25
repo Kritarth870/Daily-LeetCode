e73d867e999f99ebb16ca137d5e753812c143588
Kritarth Yadav <kritarthyadav71@gmail.com>
2026-03-25 19:49:23 +0530

    Update findPeakElement.java

---
+++ findPeakElement.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int max = Integer.MIN_VALUE;
+
+        int k = 0;
+
+        for(int i=0 ; i<nums.length ; i++){
+            if(nums[i]>=max){
+                max=nums[i];
+                k=i;
+            }
+        }
+        
+        return k;
+    }
+}
+
